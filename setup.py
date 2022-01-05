cd2fae35cdfaa3f57ee5f622d3941cabe5ff2ed9
Greg Starr <greg.starr@jhuapl.edu>
2022-01-05 14:16:24 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='trough',
+    version='',
+    packages=['trough'],
+    url='https://github.com/gregstarr/trough',
+    license='MIT',
+    author='gregstarr',
+    author_email='gregstarr2@gmail.com',
+    description=''
+)
