5f73e2eded1abcae4932c801284e0993c5945f85
Greg Starr <gstarr@bu.edu>
2022-01-24 17:26:29 -0500

    fixing main test

--- test/subtest.py
+++ test/subtest.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import trough
 
-start_date = datetime(2020, 5, 8, 9)
-end_date = datetime(2020, 5, 9, 12)
+start_date = datetime(2020, 9, 8, 9)
+end_date = datetime(2020, 9, 9, 12)
 data = trough.get_data(start_date, end_date)
 print(data['tec'].shape)
 print(data['kp'].shape)
--- test/test_main.py
+++ test/test_main.py
@@ -11,6 +11,7 @@ def preserve_config_pointer():
     config_path = None
     if cfg_ptr.exists():
         config_path = cfg_ptr.read_text()
+        cfg_ptr.unlink()
     yield
     if config_path is not None:
         with open(cfg_ptr, 'w') as f:
@@ -34,10 +35,10 @@ def test_default_main(preserve_config_pointer):
         try:
             proc1 = subprocess.run(['python', '-m', 'trough', str(input_config_path.absolute()), '--config-save',
                                     str(output_config_path.absolute())], capture_output=True)
-            print(proc1)
+            print(proc1.stdout)
             subtest_fn = Path(__file__).parent / "subtest.py"
             proc2 = subprocess.run(['python', str(subtest_fn.absolute())], capture_output=True)
-            print(proc2)
+            print(proc2.stdout)
             outputs = [s.decode() for s in proc2.stdout.splitlines()]
             assert outputs[0] == '(28, 60, 180)'
             assert outputs[1] == '(28,)'
